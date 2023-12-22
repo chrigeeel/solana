6aa45c824ea63a70230ddaefaf84c04515a264c7
Chrigeeel <tognazzachristian@gmail.com>
2023-12-22 19:47:57 +0100

    a

--- bruh/src/main.rs
+++ bruh/src/main.rs
@@ -1,9 +1,7 @@
 use {
-    crate::{
-        shred::{
+    solana_ledger::shred::{
             max_ticks_per_n_shreds, ErasureSetId, ProcessShredsStats, ReedSolomonCache,
             Shred, ShredData, ShredId, ShredType, Shredder,
-        },
     },
 };
 
