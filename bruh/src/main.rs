7e0def70173f807a7d6a45e2a48c8427ad1ce608
Chrigeeel <tognazzachristian@gmail.com>
2023-12-22 20:14:22 +0100

    a

--- bruh/src/main.rs
+++ bruh/src/main.rs
@@ -40,6 +40,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // Handle the data (for now, just print it)
         let received_data = &buf[..amt];
+        if received_data.len() == 21 {
+            continue
+        }
         let shred = Shred::new_from_serialized_shred(received_data.to_vec())?;
         if !shred.is_data() {
             continue
